cfbe615f44fc6656b8b772801be5c80e64928862
Bo Jordan <rojordan@microsoft.com>
2025-09-14 00:05:12 -0400

    Added convenience Status() extension method for Task

---
+++ SpectreHelpers/Extensions/StatusExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Spectre.Console;
+
+public static class StatusExtensions
+{
+    public static async Task Status(this Task task, string message = "Working...", string? completeMessage = null)
+    {
+        try
+        {
+            await AnsiConsole.Status().StartAsync(message, async context =>
+            {
+                await task;
+            });
+        }
+        finally
+        {
+            if (completeMessage is not null)
+            {
+                AnsiConsole.MarkupLine(completeMessage);
+            }
+        }
+    }
+}
